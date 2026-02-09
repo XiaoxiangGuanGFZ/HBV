beff95c78aa7a77cec261b9eb9c45a1c76e7e410
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2026-02-09 21:07:03 +0100

    add comments in soil module

--- scr/Func_Soil.c
+++ scr/Func_Soil.c
@@ -18,10 +18,23 @@ void Routine_soil(
     double P_LP
 )
 {
-    //The distribution of infiltration water: sum of snowmelt and rainfall
+    /**************
+     * water balance in the soil box
+     * -- inputs:
+     * I:       water flux to the soil box, source from rainfall, and snowmelt water. 
+     * Epot:    potential evaporation, mm/d
+     * SM_pre: the soil water content at the current time step
+     * -- parameters:
+     * P_FC:    Maximum soil moisture storage, mm
+     * P_LP:    Fraction of FC at which potential evapotranspiration is reduced, 
+     * P_BETA:  Shape Parameter for Runoff Generation, which controls the nonlinearity of runoff generation from the soil moisture zone
+     * ***********/
 
-    *flux_GW = I * pow(SM_pre / P_FC, P_BETA); // groundwater recharge
-    *flux_Soil = I - *flux_GW; // soil recharge
+    // The distribution of infiltration water: sum of snowmelt and rainfall
+    // a certain amount of water goes to groundwater zone (boxes); 
+    // remaining water goes to the soil box. 
+    *flux_GW = I * pow(SM_pre / P_FC, P_BETA); // groundwater recharge, >= 0.0 
+    *flux_Soil = I - *flux_GW; // soil recharge, >= 0.0
     
     // evaporation from soil
     double tmp;
@@ -30,18 +43,19 @@ void Routine_soil(
     {
         tmp = 1.0;
     }
-    *Eact = Epot * tmp; // the actual estimated evaporation
+    *Eact = Epot * tmp; // the actual estimated evaporation, from soil box (moisture)
 
+    // compute the water balance in the soil box
     tmp = *flux_Soil + SM_pre - *Eact;
     if (tmp > P_FC)
     {
         // the soil is full (with SM reaching its capacity P_FC)
-        *flux_GW += tmp - P_FC; // excess water into groundwater
+        *flux_GW += tmp - P_FC; // *** excess water goes to the groundwater ***
         *SM_post = P_FC;
     } 
     else if (tmp < 0.0)
     {
-        // not enough soil water for evaporation, 
+        // *** not enough soil water for evaporation *** 
         *Eact = *flux_Soil + SM_pre;
         *SM_post = 0.0;
     } else {
